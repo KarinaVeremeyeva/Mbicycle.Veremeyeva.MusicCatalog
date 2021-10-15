1552737221e19fc61164b211847110f223bb5307
KarinaVeremeyeva <57318127+KarinaVeremeyeva@users.noreply.github.com>
2021-10-15 17:48:49 +0300

    Implement add/edit performer components

--- src/MusicCatalog-AngularClient/src/app/performer/add-performer/add-performer.component.ts
+++ src/MusicCatalog-AngularClient/src/app/performer/add-performer/add-performer.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Performer } from '../../_models/performer';
+import { PerformerService } from '../../_services/performer.service';
+
 @Component({
   selector: 'app-add-performer',
   templateUrl: './add-performer.component.html'
 })
 export class AddPerformerComponent implements OnInit {
+  performer: Performer = new Performer();
+  submitted = false;
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private performerService: PerformerService) { }
 
   ngOnInit(): void {
   }
 
+  createPerformer(): void {
+    this.performerService.postPerformer(this.performer)
+      .subscribe(() => {
+        this.submitted = true;
+      },
+        err => {
+        this.errorMessage = err;
+      });
+  }
 }
--- src/MusicCatalog-AngularClient/src/app/performer/edit-performer/edit-performer.component.ts
+++ src/MusicCatalog-AngularClient/src/app/performer/edit-performer/edit-performer.component.ts
@@ -1,14 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators} from '@angular/forms';
+import { ActivatedRoute, Router} from '@angular/router';
+
+import { Performer } from '../../_models/performer';
+import { PerformerService } from '../../_services/performer.service';
 
 @Component({
   selector: 'app-edit-performer',
   templateUrl: './edit-performer.component.html'
 })
 export class EditPerformerComponent implements OnInit {
+  currentPerformer: Performer = new Performer();
+  editForm;
 
-  constructor() { }
+  constructor(
+    private performerService: PerformerService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.editForm = this.formBuilder.group({
+      name: ['', Validators.required]
+    });
+
+    this.currentPerformer.performerId = this.route.snapshot.params['id'];
+
+    this.performerService.getPerformer(this.currentPerformer.performerId)
+      .subscribe((response: Performer) => {
+        this.currentPerformer = response;
+        this.editForm.controls["name"].setValue(this.currentPerformer.name)
+      });
   }
 
+  onSubmit(formData) {
+    formData.value.performerId = this.currentPerformer.performerId;
+
+    this.performerService.putPerformer(formData.value).subscribe(() => {
+      this.router.navigateByUrl('performers').then();
+    });
+  }
 }
