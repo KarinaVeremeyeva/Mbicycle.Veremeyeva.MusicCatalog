9be9fbc9c240364a5bbcac4907be0ebb8b122104
Karina Veremeyeva <57318127+KarinaVeremeyeva@users.noreply.github.com>
2021-11-05 17:41:49 +0300

    Update web api authorization settings and connection string

--- src/MusicCatalog.WebApi/JwtTokenAuth/JwtAuthenticationOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Authentication;
-
-namespace MusicCatalog.WebApi.JwtTokenAuth
-{
-    public class JwtAuthenticationOptions : AuthenticationSchemeOptions
-    {
-    }
-}
