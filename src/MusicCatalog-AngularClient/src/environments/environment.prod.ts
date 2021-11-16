962fc746d3d4068a51e997567f10d8ce5a393f7b
Karina Veremeyeva <karina.veremeyeva@gmail.com>
2021-11-16 18:00:55 +0300

    Update docker compose, add yaml files

--- src/MusicCatalog-AngularClient/src/environments/environment.prod.ts
+++ src/MusicCatalog-AngularClient/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  userApiUrl: 'http://localhost:5000/api',
-  webApiUrl: 'http://localhost:5001/api'
+  userApiUrl: 'http://identity-api:5000',
+  webApiUrl: 'http://web-api:5001'
+  //userApiUrl: 'http://localhost:5000/api',
+  //webApiUrl: 'http://localhost:5001/api'
 };
