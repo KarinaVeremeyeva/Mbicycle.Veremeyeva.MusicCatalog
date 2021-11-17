b3d5f3fe8c74807c86343cc450969565aa5d6e2b
Karina Veremeyeva <karina.veremeyeva@gmail.com>
2021-11-17 17:52:26 +0300

    Update client path and connection string

--- src/MusicCatalog-AngularClient/src/environments/environment.ts
+++ src/MusicCatalog-AngularClient/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  userApiUrl: 'http://identity-api:5000',
-  webApiUrl: 'http://web-api:5001'
+  userApiUrl: 'http://localhost/identity-api/api',
+  webApiUrl: 'http://localhost/web-api/api'
   //userApiUrl: 'http://localhost:5000/api',
   //webApiUrl: 'http://localhost:5001/api'
 };
